dbc200b9e6bd841ccddad2814e32bafbde4ea0a9
TracyTN <tracytn0411@gmail.com>
2019-11-02 13:51:49 -0700

    Updates

---
+++ precache-manifest.d7a40ce1d8643698296768d0057aa439.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344b69af1eeba46ed47e79853dd6babb",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "b1bbff9e071ff6a6eb9c",
+    "url": "/Clicky-Game/static/css/main.ea1849bc.chunk.css"
+  },
+  {
+    "revision": "858337689f8457637e8e",
+    "url": "/Clicky-Game/static/js/2.05bcf8b3.chunk.js"
+  },
+  {
+    "revision": "b1bbff9e071ff6a6eb9c",
+    "url": "/Clicky-Game/static/js/main.fbc92bf7.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  }
+]);
